4621b9fadebddb4384442241bb48b60101081616
Gourav S B <gouravsb1@gmail.com>
2015-05-20 10:31:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,12 @@
+##There are two separate functions defined as it was done in the given
+##example of caching the mean of a vector.
+##Function 1.) is called the makeCacheMatrix which makes the matrix special 
+##Function 2.) is called the cacheSolve which checks for the existence of the inverse of the new
+##matrix and caches it or calculates it
+
+
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  m <- NULL         ## making the matrix null
   set <- function(y) {
     x <<- y
     m <<- NULL
@@ -12,12 +19,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
   m <- x$getinverse()
-  if(!is.null(m)) {
+  if(!is.null(m)) {   ## conditional statement checking the existence of the inverse of the new matrix 
     message("getting cached data")
-    return(m)
+    return(m)         ## If value is TRUE then it returns the cached inverse and ends the function
   }
   data <- x$get()
-  m <- solve(data, ...)
+  m <- solve(data, ...)   ## Solving the inverse of the matrix using solve()
   x$setinverse(m)
   m
 }
